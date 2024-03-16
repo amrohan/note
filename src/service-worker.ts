4c5a30bac4b1b06d7bdf0e39d6939907a25be446
amrohan <rohanxsalunkhe@gmail.com>
2024-03-16 19:58:02 +0530

    feat/ fix pwa offline issue

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -5,30 +5,66 @@ declare const self: ServiceWorkerGlobalScope;
 
 import { build, files, version } from '$service-worker';
 
+const CACHE = `cache-${version}`;
+
+const ASSETS = [...build, ...files];
+
 self.addEventListener('install', (event) => {
-	event.waitUntil(
-		caches.open(version).then((cache) => {
-			cache.addAll(build.concat(files));
-		})
-	);
+	async function preCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUntil(preCache());
 });
 
 self.addEventListener('activate', (event) => {
-	event.waitUntil(
-		caches.keys().then(async (keys) => {
-			for (const key of keys) {
-				if (key !== version) await caches.delete(key);
-			}
-		})
-	);
+	async function clearOldCache() {
+		const keys = await caches.keys();
+		await Promise.all(
+			keys.map((key) => {
+				if (key !== CACHE) {
+					return caches.delete(key);
+				}
+			})
+		);
+	}
+
+	event.waitUntil(clearOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-	event.respondWith(
-		caches.match(event.request).then((response) => {
-			return response || fetch(event.request);
-		})
-	);
+	if (event.request.method !== 'GET') return;
+
+	/**
+	 * Handles the response logic for the service worker.
+	 * @returns {Promise<Response>} The response to be sent back to the client.
+	 */
+	async function respond() {
+		const url = new URL(event.request.url);
+		const cache = await caches.open(CACHE);
+
+		if (ASSETS.includes(url.pathname)) {
+			const response = await cache.match(url.pathname);
+			if (response) return response;
+		}
+
+		try {
+			const response = await fetch(event.request);
+			const isNotExtension = url.protocol.startsWith('http') && !url.pathname.includes('.');
+			const isSuccessful = response.status === 200 || response.status === 0;
+			if (isNotExtension && isSuccessful) {
+				cache.put(event.request, response.clone());
+			}
+			return response;
+		} catch (error) {
+			//fallback to cache
+			const cachedResponse = await cache.match(url.pathname);
+			if (cachedResponse) return cachedResponse;
+		}
+		return new Response(null, { status: 404 });
+	}
+	event.respondWith(respond());
 });
 
 self.addEventListener('message', (event) => {
@@ -36,5 +72,3 @@ self.addEventListener('message', (event) => {
 		self.skipWaiting();
 	}
 });
-
-// Path: src/routes/index.svelte
